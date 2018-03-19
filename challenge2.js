fd3dfb2103d2ac0aeb4cc8a7f90c4f6d893dc807
zuhalozcann <zuutje1994>
2018-03-19 12:00:38 +0100

    Upload js code for the clock

---
+++ challenge2.js
@@ -0,0 +1,26 @@
+var tween = TweenMax.to($box, 1, {
+x: '-=50', // Tween to the current x value minus 50
+y: '+=50', // Tween to the current y value plus 50
+onComplete: myFunction,
+ease:Back.easeOut
+});
+tl.add(tween, 2); // Add tween 2 seconds into the timeline
+
+
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
